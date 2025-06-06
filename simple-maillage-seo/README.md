35abefd733da3801215900574a2f164971e53a49
Sulfamique <valentin.mda@gmail.com>
2025-06-06 17:11:00 +0200

    Restructure plugin files

--- simple-maillage-seo/README.md
+++ simple-maillage-seo/README.md
@@ -2,12 +2,13 @@
 
 Ce plugin WordPress permet de créer facilement des liens internes sur tout le site. Il ajoute un tableau de bord dans l'administration où vous pouvez définir des couples **mot clé / URL**. Chaque fois qu’un mot clé est trouvé dans un contenu, il est automatiquement remplacé par un lien vers l’URL définie.
 
-Le dossier du plugin contient également une feuille de style `admin.css` pour améliorer l'affichage du tableau de bord.
+Le dossier du plugin contient également une feuille de style `admin/css/admin.css` pour améliorer l'affichage du tableau de bord.
 
 ## Installation
 
 1. Copier le dossier `simple-maillage-seo` dans le répertoire `wp-content/plugins/` de votre site WordPress.
 2. Activer l'extension "Simple Maillage SEO" depuis l'administration WordPress.
+3. Vérifiez que l'extension PHP **DOM** est installée : elle est nécessaire pour analyser le contenu et insérer les liens.
 
 ## Utilisation
 
--- simple-maillage-seo/simple-maillage-seo.php
+++ simple-maillage-seo/simple-maillage-seo.php
@@ -13,152 +13,20 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Quitter si accès direct
 }
 
-class SimpleMaillageSEO {
-
-    private static $option_key = 'sms_keyword_links';
-
-    public static function init() {
-        add_action( 'admin_menu', array( __CLASS__, 'add_admin_menu' ) );
-        add_action( 'admin_init', array( __CLASS__, 'handle_form_submission' ) );
-        add_action( 'admin_enqueue_scripts', array( __CLASS__, 'enqueue_admin_assets' ) );
-        add_filter( 'the_content', array( __CLASS__, 'apply_links' ) );
-    }
-
-    public static function get_links() {
-        $links = get_option( self::$option_key, array() );
-        return is_array( $links ) ? $links : array();
-    }
-
-    public static function save_links( $links ) {
-        update_option( self::$option_key, $links );
-    }
-
-    public static function enqueue_admin_assets( $hook ) {
-        if ( $hook !== 'toplevel_page_simple-maillage-seo' ) {
-            return;
-        }
-        wp_enqueue_style( 'sms-admin', plugins_url( 'admin.css', __FILE__ ) );
-    }
-
-    public static function add_admin_menu() {
-        add_menu_page(
-            'Simple Maillage SEO',
-            'Maillage SEO',
-            'manage_options',
-            'simple-maillage-seo',
-            array( __CLASS__, 'admin_page' )
-        );
-    }
-
-    public static function handle_form_submission() {
-        if ( ! current_user_can( 'manage_options' ) ) {
-            return;
-        }
-
-        if ( isset( $_POST['sms_add'] ) && check_admin_referer( 'sms_save_links' ) ) {
-            $keyword = sanitize_text_field( $_POST['sms_keyword'] );
-            $url     = esc_url_raw( $_POST['sms_url'] );
-            if ( $keyword && $url ) {
-                $links = self::get_links();
-                $links[ $keyword ] = $url;
-                self::save_links( $links );
-            }
-        }
-
-        if ( isset( $_POST['sms_delete'] ) && isset( $_POST['sms_keyword_delete'] ) && check_admin_referer( 'sms_save_links' ) ) {
-            $del = sanitize_text_field( $_POST['sms_keyword_delete'] );
-            $links = self::get_links();
-            if ( isset( $links[ $del ] ) ) {
-                unset( $links[ $del ] );
-                self::save_links( $links );
-            }
-        }
-    }
-
-    public static function admin_page() {
-        ?>
-        <div class="wrap">
-            <h1>Simple Maillage SEO</h1>
-            <form method="post">
-                <?php wp_nonce_field( 'sms_save_links' ); ?>
-                <table class="widefat sms-table">
-                    <thead>
-                        <tr>
-                            <th>Mot clef</th>
-                            <th>URL</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ( self::get_links() as $kw => $url ) : ?>
-                            <tr>
-                                <td><?php echo esc_html( $kw ); ?></td>
-                                <td><?php echo esc_url( $url ); ?></td>
-                                <td>
-                                    <button type="submit" name="sms_delete" class="button-link-delete" value="1">Supprimer</button>
-                                    <input type="hidden" name="sms_keyword_delete" value="<?php echo esc_attr( $kw ); ?>" />
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                        <tr>
-                            <td><input type="text" name="sms_keyword" /></td>
-                            <td><input type="url" name="sms_url" /></td>
-                            <td><button type="submit" name="sms_add" class="button button-primary" value="1">Valider</button></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </form>
-        </div>
-        <?php
-    }
-
-    public static function apply_links( $content ) {
-        $links = self::get_links();
-        if ( empty( $links ) ) {
-            return $content;
-        }
-
-        libxml_use_internal_errors( true );
-        $dom = new DOMDocument();
-        $encoding = get_bloginfo( 'charset' );
-        $dom->loadHTML( '<?xml encoding="' . $encoding . '"?>' . $content );
-        $xpath = new DOMXPath( $dom );
-
-        foreach ( $links as $keyword => $url ) {
-            $nodes = $xpath->query( '//text()[
-                not(ancestor::a)
-                and not(ancestor::h1)
-                and not(ancestor::h2)
-                and not(ancestor::h3)
-                and not(ancestor::h4)
-                and not(ancestor::h5)
-                and not(ancestor::h6)
-                and not(ancestor::nav)
-                and not(ancestor::header)
-                and not(ancestor::footer)
-                and not(ancestor::*[contains(@class,"sommaire") or contains(@class,"toc") or contains(@class,"breadcrumb")])
-            ]' );
-            foreach ( $nodes as $node ) {
-                if ( stripos( $node->nodeValue, $keyword ) !== false ) {
-                    $regex = '/(\b' . preg_quote( $keyword, '/' ) . '\b)/i';
-                    $new = preg_replace( $regex, '<a href="' . esc_url( $url ) . '">$1</a>', $node->nodeValue, 1 );
-                    if ( $new !== $node->nodeValue ) {
-                        $frag = $dom->createDocumentFragment();
-                        $frag->appendXML( $new );
-                        $node->parentNode->replaceChild( $frag, $node );
-                        break;
-                    }
-                }
-            }
-        }
-
-        $body = $dom->getElementsByTagName( 'body' )->item( 0 );
-        $new_content = '';
-        foreach ( $body->childNodes as $child ) {
-            $new_content .= $dom->saveHTML( $child );
-        }
-        return $new_content;
-    }
+// Le plugin repose sur l'extension PHP DOM pour manipuler le HTML
+if ( ! extension_loaded( 'dom' ) ) {
+    add_action( 'admin_notices', function() {
+        echo '<div class="notice notice-error"><p>';
+        esc_html_e( "Simple Maillage SEO requiert l'extension PHP DOM.", 'simple-maillage-seo' );
+        echo '</p></div>';
+    } );
+    return;
 }
+
+define( 'SMS_PLUGIN_FILE', __FILE__ );
+define( 'SMS_PLUGIN_DIR', plugin_dir_path( SMS_PLUGIN_FILE ) );
+define( 'SMS_PLUGIN_URL', plugin_dir_url( SMS_PLUGIN_FILE ) );
+
+require_once SMS_PLUGIN_DIR . 'includes/class-simple-maillage-seo.php';
 
 SimpleMaillageSEO::init();
